8cce3dbc9350c592ee5bd1de27464d40a38e9df5
yoigor226 <intsys517@gmail.com>
2025-05-20 13:12:10 +0000

    image

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -80,7 +80,7 @@ const Banner = () => {
             <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
               <img
                 className="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-126"
-                src="src/assets/education.jpg"
+                src="./src/assets/education.jpg"
                 alt="Environnement d'apprentissage moderne"
               />
             </div>
